e10d9b9d1a3788cf85f81cc00a88c28a1b1e1ff6
Ian Agpawa <agpawaji@gmail.com>
2016-07-16 17:49:35 -0400

    styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,10 @@
-
-
 function startPage(){
-<<<<<<< HEAD
 
 
 
 
   var turn = 0;
 
-=======
-  
-
-  
-  
-  var turn = 0;
-  
->>>>>>> origin/master
   function checkTurn() {
     if (turn % 2 === 0) {
       return "X";
@@ -23,17 +12,10 @@ function startPage(){
       return "O";
     };
   }
-<<<<<<< HEAD
 
   var customAlert = new alertBox();
 
 
-=======
-  
-  var customAlert = new alertBox();
-  
-  
->>>>>>> origin/master
   var ended = false;
   function checkArr(array) {
     if (array.every(checkAllX)) {
@@ -50,7 +32,6 @@ function startPage(){
       return false;
     };
   }
-<<<<<<< HEAD
 
   function checkAllX(element, index, array) {
     return (element === "X")
@@ -60,17 +41,6 @@ function startPage(){
     return (element === "O")
   }
 
-=======
-  
-  function checkAllX(element, index, array) {
-    return (element === "X")
-  }
-  
-  function checkAllO(element, index, array) {
-    return (element === "O")
-  }
-  
->>>>>>> origin/master
   function check(thing) {
     var arr = [];
     thing.each(function() {
@@ -80,20 +50,12 @@ function startPage(){
       resetAll();
     }
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function checkAll() {
     var allCombos = selectors();
     allCombos.forEach(check);
   };
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function selectors() {
     var threes = [".L", ".M", ".R", ".T", ".MM", ".B", ".DR", ".DL"];
     var formatted = threes.map(function(el) {
@@ -101,11 +63,7 @@ function startPage(){
     })
     return formatted;
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function displayTurn() {
     if (checkTurn(turn) === "X") {
       $("#X").show();
@@ -115,28 +73,16 @@ function startPage(){
       $("#X").css("display", "none");
     };
   };
-<<<<<<< HEAD
 
   displayTurn();
 
-=======
-  
-  displayTurn();
-  
->>>>>>> origin/master
   var color = {
     "X": "#8E0000",
     "O": "#00004C"
   };
-<<<<<<< HEAD
 
   var shadow = "-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white";
 
-=======
-  
-  var shadow = "-1px 0 white, 0 1px white, 1px 0 white, 0 -1px white";
-  
->>>>>>> origin/master
   $(".cell").on("click", function() {
     square = $(this);
     if (square.text() === "") {
@@ -157,12 +103,6 @@ function startPage(){
         computer = true;
         checkState();
       } else if ((turn >= 9)) {
-<<<<<<< HEAD
-=======
-        /*
-        alert("Game has ended. No one won.  Play another.");
-        $("#diag-box-body").css('font-size', '10px');*/
->>>>>>> origin/master
         resetAll();
       } else if (checkAll() !== undefined) {
         checkAll();
@@ -170,11 +110,7 @@ function startPage(){
       }
     };
   });
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function resetAll() {
     $(".cell").text("");
     turn = 0;
@@ -189,29 +125,17 @@ function startPage(){
     $("#diag-overlay").css('display', 'none');
     $("#diag-box").css('display', 'none')
   }
-<<<<<<< HEAD
 
   var allCells = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var occupiedByX = [];
   var occupiedByO = [];
 
-=======
-  
-  var allCells = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var occupiedByX = [];
-  var occupiedByO = [];
-  
->>>>>>> origin/master
   function deleteCell(stringN) {
     pos = allCells.indexOf(stringN);
     allCells.splice(pos, 1);
     return allCells;
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   var combos = {
     ".L": ['1', '4', '7'],
     ".M": ['2', '5', '8'],
@@ -222,11 +146,7 @@ function startPage(){
     ".DR": ['1', '5', '9'],
     ".DL": ['3', '5', '7']
   };
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function whichList(val) {
     if (val === "X") {
       return [occupiedByX, occupiedByO];
@@ -234,11 +154,7 @@ function startPage(){
       return [occupiedByO, occupiedByX];
     }
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function getMoveArray() {
     var player = whichList(checkTurn())[0];
     var opponent = whichList(checkTurn())[1];
@@ -264,11 +180,7 @@ function startPage(){
     });
     return [goAhead, prevent];
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function checkIncluded(element, array) {
     if (array.indexOf(element) !== -1) {
       return true;
@@ -276,31 +188,19 @@ function startPage(){
       return false;
     };
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function removedArr(element, array) {
     var pos = array.indexOf(element);
     array.splice(pos, 1);
     return array;
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function getMoveRandom() {
     var length = allCells.length;
     var pos = Math.floor((Math.random() * length) + 1);
     return allCells[pos];
   };
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function getMove(array) {
     var goAhead = array[0];
     var prevent = array[1];
@@ -321,11 +221,7 @@ function startPage(){
     };
     return (available() || possibilities[0] || getMoveRandom());
   }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
   function available() {
     if (checkIncluded("5", allCells)) {
       return "5";
@@ -333,15 +229,9 @@ function startPage(){
       return false;
     };
   };
-<<<<<<< HEAD
 
   var computer = false;
 
-=======
-  
-  var computer = false;
-  
->>>>>>> origin/master
   function computerMove() {
     var arrays = getMoveArray();
     var nextMove = getMove(arrays);
@@ -352,50 +242,29 @@ function startPage(){
     $(id).css("color", color[checkTurn()]);
     $(id).css("text-shadow", shadow);
   };
-<<<<<<< HEAD
 
 
-=======
-  
-  
->>>>>>> origin/master
   function checkState() {
     if (computer === true) {
       computerMove();
       turn += 1;
       if (checkAll() != undefined) {
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> origin/master
         checkAll();
         resetAll();
       } else if ((turn >= 9) && (ended == false)){
         checkAll();
-<<<<<<< HEAD
         customAlert.render("Game has ended in a draw.");
-=======
-        customAlert.render("Game has ended. No one won.");
->>>>>>> origin/master
         $("#diag-box-body").css('background-color', "#010303");
       };
       computer = false;
     }
   }
-<<<<<<< HEAD
 
   $("#reset-button").on("click", function() {
     resetAll();
   });
 
-=======
-  
-  $("#reset-button").on("click", function() {
-    resetAll();
-  });
-  
->>>>>>> origin/master
   $("#switch-player").on("click", function() {
     computer = true;
     checkState();
@@ -403,31 +272,19 @@ function startPage(){
     displayTurn();
     checkAll();
   });
-<<<<<<< HEAD
 
   function alertBox() {
     this.render = function(msg) {
       var winW = ($(window).width()).toString();
       var halfW = ((winW/2) - (490 * 0.5)).toString() +"px";
       console.log(halfW);
-=======
-  
-  function alertBox() {
-    this.render = function(msg) {
-      var winW = ($(window).width()).toString();
->>>>>>> origin/master
       var winH = ($(window).height()).toString();
       var diagOverlay = $("#diag-overlay");
       var diagBox = $("#diag-box");
       diagOverlay.css("display", "block");
       diagOverlay.css("height", winH + "px");
-<<<<<<< HEAD
       diagBox.css("margin-top", "150px");
       diagBox.css("margin-left", halfW);
-=======
-      diagBox.css("margin-left", ((winW/2) - (500 * 0.5)).toString() +"px");
-      diagBox.css("margin-top", "150px");
->>>>>>> origin/master
       diagBox.css('display', "block");
       $("#diag-box-head").text("GAME OVER");
       $("#play-again").on('click', function() {
@@ -436,7 +293,6 @@ function startPage(){
       $("#diag-box-body").text(msg);
     }
   }
-<<<<<<< HEAD
 
 }
 
@@ -444,11 +300,3 @@ function startPage(){
 
 $(document).ready(startPage);
 $(document).on('page:load', startPage);
-=======
-
-}
-
-
-$(document).ready(startPage);
-$(document).on('page:load', startPage);
->>>>>>> origin/master
